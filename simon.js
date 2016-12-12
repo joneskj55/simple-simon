afce06f9a8433e4c1296b78cabb6cbdab35dd806
Kevin Jones <joneskj55@gmail.com>
2016-12-12 09:45:52 -0600

    start button illuminates random box

--- simon.js
+++ simon.js
@@ -3,45 +3,41 @@
  */
 
 "use strict";
-var computerPattern = [];
-var userPattern = [];
-var level = 0;
-var matchPatterns = false;
 
-function addPattern() {
-    //set pattern
-}
+// var startGame = false;
+var randomPattern = [];
+// var matchPattern = ;
+// var successfulDuplication = ;
+// var failedDuplication = ;
+// var addToPattern = ;
+    var userPattern = [];
 
-function playPattern() {
-    //match
-}
 $(document).ready(function () {
     var squareArray = ["#red-div", "#blue-div", "#green-div", "#yellow-div"];
     console.log(squareArray.length);
     var illuminateRandomSquare = randomNumber();
-    console.log(illuminateRandomSquare);
+    console.log("Random Square is: " + illuminateRandomSquare);
 
     //-------GENERATE RANDOM NUMBER----------//
-    function randomNumber(min, max) {
+    function randomNumber() {
         return Math.floor(Math.random() * (3 - 0 + 1)) + 0;
     }
 
-    console.log(randomNumber());
 
     //------------START GAME----------//
     $("#start-game").click(function () {
-        $(".square").fadeOut(100).fadeIn(100);
+        var computerPattern = squareArray[randomNumber()];
+        $(computerPattern).fadeOut(100).fadeIn(100);
+        if (userPattern == computerPattern) {
+            return userPattern.append(randomNumber());
+        }
     });
+
+    // on click of correct box pattern should repeat what was already output and add one box to the pattern
+
 
     //-------ANIMATE SQUARE UPON CLICKING------//
     $(".square").click(function () {
         $(this).fadeOut(100).fadeIn(100);
     });
-
-
-    //--------ASSIGN RANDOM NUMBER TO SQUARE----//
-//        function assignedSquare(number, box) {
-//            $(".square")[randomNumber[0]];
-//        }
-//        console.log(assignedSquare());
 });
